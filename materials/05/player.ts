c87c53924fc85928677c726189757146f5ee31f4
Stephen Samuels <stevesam1927@gmail.com>
2021-07-31 12:47:59 +0530

    materials updated fror module 5

--- materials/05/player.ts
+++ materials/05/player.ts
@@ -1,9 +1,7 @@
-/// <reference path="person.ts" />
-
 class Player implements Person {
-  name: string;
-  age: number;
-  highScore: number;
+  name!: string;
+  age?: number;
+  highScore!: number;
 
   formatName() {
     return this.name.toUpperCase();
